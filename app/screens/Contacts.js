bda575990ff9253b2e702ca6d492f6684338eb51
Moisés Ñañez <mnanez@able.co>
2018-09-23 11:42:36 -0500

    Add screens

---
+++ app/screens/Contacts.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class Contacts extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Contacts screen</Text>
+      </View>
+    );
+  }
+}
---
+++ app/screens/NewContact.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class NewContact extends Component {
+  render() {
+    return (
+      <View>
+        <Text>NewContact screen</Text>
+      </View>
+    );
+  }
+}
