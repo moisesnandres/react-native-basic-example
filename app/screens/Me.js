6fb34b9ab1be581dc792becee38a1551dd85326d
Moisés Ñañez <mnanez@able.co>
2018-09-23 17:52:23 -0500

    Render Me screen

--- app/screens/Me.js
+++ app/screens/Me.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ScrollView } from "react-native";
+
+import { Header, Actions, Info } from "../components/UserDetails";
+import { PrimaryButton } from "../components/Buttons";
+import colors from "../config/colors";
+import { me } from "../config/data";
 
 export default class Me extends Component {
   render() {
     return (
-      <View>
-        <Text>Me screen</Text>
-      </View>
+      <ScrollView style={{ backgroundColor: colors.background }}>
+        <Header {...me} />
+        <PrimaryButton label="Edit Profile" onPress={() => null} />
+        <Actions {...me} />
+        <Info {...me} />
+      </ScrollView>
     );
   }
 }
