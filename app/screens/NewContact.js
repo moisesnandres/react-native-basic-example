c21a12317fcf5ac05cf085ea7f90de25fdaa00fa
Moisés Ñañez <mnanez@able.co>
2018-09-23 19:03:59 -0500

    Add inputs form NewContact screen

--- app/screens/NewContact.js
+++ app/screens/NewContact.js
@@ -1,12 +1,62 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import colors from "../config/colors";
+import { TextInput } from "../components/TextInput";
+import { PrimaryButton } from "../components/Buttons";
+
+const fields = [
+  { placeholder: "First Name", stateKey: "firstName" },
+  { placeholder: "Last Name", stateKey: "lastName" },
+  { placeholder: "Email", stateKey: "email", keyboardType: "email-address" },
+  { placeholder: "Mobile Phone", stateKey: "mobilePhone" },
+  { placeholder: "Home Phone", stateKey: "homePhone" },
+  { placeholder: "City", stateKey: "city" },
+  { placeholder: "Birthday", stateKey: "birthday" },
+  { placeholder: "Registered", stateKey: "registered" },
+  { placeholder: "Username", stateKey: "username" }
+];
 
 export default class NewContact extends Component {
+  state = {};
+
+  onInputChange = (text, stateKey) => {
+    const mod = {};
+    mod[stateKey] = text;
+    this.setState(mod);
+  };
+
+  handleSubmit = (index, override = false) => {
+    if (index === fields.length - 1 || override) {
+      alert("Submit");
+    } else {
+      const nextField = fields[index + 1];
+      this[nextField.stateKey].focus();
+    }
+  };
+
   render() {
     return (
-      <View>
-        <Text>NewContact screen</Text>
-      </View>
+      <KeyboardAwareScrollView style={{ backgroundColor: colors.background }}>
+        {fields.map((field, index) => (
+          <TextInput
+            key={field.stateKey}
+            placeholder={field.placeholder}
+            onChangeText={text => this.onInputChange(text, field.stateKey)}
+            returnKeyType={index === fields.length - 1 ? "done" : "next"}
+            onSubmitEditing={() => this.handleSubmit(index)}
+            ref={input => (this[field.stateKey] = input)}
+            {...field}
+          />
+        ))}
+        <View style={{ marginTop: 20 }}>
+          <PrimaryButton
+            label="Save"
+            onPress={() => this.handleSubmit(0, true)}
+          />
+        </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
